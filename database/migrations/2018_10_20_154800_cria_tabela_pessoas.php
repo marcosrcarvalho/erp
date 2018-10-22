0d9014a9800c92c85327fbe30632b402538114b6
marcoscarvalho <marcosr.carvalho@globo.com>
2018-10-22 17:16:23 -0300

    Alterado migration cria_tabela_pessoa

--- database/migrations/2018_10_20_154800_cria_tabela_pessoas.php
+++ database/migrations/2018_10_20_154800_cria_tabela_pessoas.php
@@ -6,11 +6,6 @@ use Illuminate\Database\Migrations\Migration;
 
 class CriaTabelaPessoas extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('pessoas', function (Blueprint $table) {
